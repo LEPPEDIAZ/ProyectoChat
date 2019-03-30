5f39352d36f7258a378195923f8e2aa3400638cb
Unknown <dia151378@uvg.edu.gt>
2019-03-29 21:00:02 -0600

    debbuging transfer
    part 1

--- comunicacion/transfer_functions.cpp
+++ comunicacion/transfer_functions.cpp
@@ -1,14 +1,28 @@
-void enviar_mensaje(string mensaje, int socket) {
-    int message_len_bytes = str.length(mensaje);
-    send(socket, mensaje, message_len_bytes, 0);
-}
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sys/socket.h>
 
-string recibir_mensaje(int socket) {
-    char buffer[256];
-    bzero(buffer, 256);
+using namespace std;
 
-    string response = read(socket, buffer, 255);
-    if (response < 0) error("ERROR reading from socket");
-    return str(response);
-}
+class Comunicacion {
+public:
+	
 
+	void enviar_mensaje(string mensaje, int socket) {
+	    int message_len_bytes = str.length(mensaje);
+	    send(socket, mensaje, message_len_bytes, 0);
+	}
+/**
+	string recibir_mensaje(int socket) {
+	    char buffer[256];
+	    bzero(buffer, 256);
+
+	    string response = read(socket, buffer, 255);
+	    if (response < 0) error("ERROR reading from socket");
+	    return str(response);
+	}
+
+*/
+	
+};
