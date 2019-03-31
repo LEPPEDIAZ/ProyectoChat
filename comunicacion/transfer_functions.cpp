268ca32174a7279e8e8e5bdcf1745821bffe935d
BALJ55 <byronamh@gmail.com>
2019-03-30 23:44:44 -0600

    funcionamiento de port io probado

--- comunicacion/transfer_functions.cpp
+++ comunicacion/transfer_functions.cpp
@@ -6,16 +6,17 @@
 using namespace std;
 
 void enviar_mensaje(string mensaje, int socket) {
-    int message_len_bytes = str.length(mensaje);
-    send(socket, mensaje, message_len_bytes, 0);
+    int message_len_bytes = mensaje.length();
+    send(socket, &mensaje, message_len_bytes, 0);
 }
 
 string recibir_mensaje(int socket) {
     char buffer[256];
     bzero(buffer, 256);
 
-    string response = read(socket, buffer, 255);
-    if (response.size() < 0)
+    int response_len = read(socket, buffer, 255);
+    if (response_len < 0)
         return "error al recibir el mensaje";
-    return response;
+    string mensaje = buffer;
+    return mensaje;
 }
