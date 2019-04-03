5db42818d687853b4dbfd152d676f4bf48175ae1
byronamh <byronamh@gmail.com>
2019-04-03 13:35:09 -0600

    server check implemented, pendiente parseo de requests

--- server.cpp
+++ server.cpp
@@ -28,14 +28,14 @@ using namespace std;
 
 User_Manager users[MAX_USERS_CONNECTED];
 struct thread_data {
-	int sockID;
+    int sockID;
 };
 
 void *handle_connection(void *sock_arg) {
     struct thread_data *my_data;
     my_data = (struct thread_data *) sock_arg;
     int sockID = my_data->sockID;
-    cout<<"thread in!"<< sockID << endl;
+    cout << "thread in!" << sockID << endl;
     if (sockID < 0) {
         Mensaje error_connect = new Mensaje(1);
         error_connect.error_connection_json(1, "no se puedo conectar al servidor");
@@ -43,10 +43,8 @@ void *handle_connection(void *sock_arg) {
         cerr << "Error aceptando el request del client" << endl;
         exit(1);
     }
-    cout << "Cliente conectado!" << endl;
-    cout << "Cliente socket: " << sockID<< endl;
+    cout << "Ahora escuchando para cliente en socket: " << sockID << endl;
     while (1) {
-
         string mensaje = recibir_mensaje(sockID);
         if (mensaje != "3312wazo") {
             printf("hay un mensaje en cola!\n");
@@ -106,27 +104,27 @@ int main(int argc, char *argv[]) {
 //    int newSd = accept(sockSd, (struct sockaddr *) &newSockAddr, &newSockAddrSize);
     int accepted;
     while ((accepted = accept(sockSd, (struct sockaddr *) &newSockAddr, &newSockAddrSize)) > 0) {
-        cout<<"mensaje recibido!"<<endl;
+        cout << "Nuevo usuario conectado!" << endl;
         string mensaje = recibir_mensaje(accepted);
-        cout<<mensaje<<endl;
+        cout << mensaje << endl;
         auto mensaje_parseado = json::parse(mensaje);
-	int code = mensaje_parseado["code"];
-	if(code == 0){
-		string new_username = mensaje_parseado["data"]["username"];
-		unsigned long int curr_time = time(NULL);
+        int code = mensaje_parseado["code"];
+        if (code == 0) {
+            string new_username = mensaje_parseado["data"]["username"];
+            unsigned long int curr_time = time(NULL);
 
-		int user_stack_id = users->add_user(new_username, 1, 1, accepted);
-		cout << user_stack_id << endl;
-		if (user_stack_id != -1) {
-			pthread_t thr;
-			struct thread_data td;
-			td.sockID = users->get_user_socket(user_stack_id);
-			cout << "Cliente socket: " << users->get_user_socket(user_stack_id)<< endl;
-			pthread_create(&thr, NULL, handle_connection, (void *) &td); 
- 	    
-		}
-		
-	}
+            int user_stack_id = users->add_user(new_username, 1, 1, accepted);
+            cout << user_stack_id << endl;
+            if (user_stack_id != -1) {
+                pthread_t thr;
+                struct thread_data td;
+                td.sockID = users->get_user_socket(user_stack_id);
+                cout << "Cliente socket: " << users->get_user_socket(user_stack_id) << endl;
+                pthread_create(&thr, NULL, handle_connection, (void *) &td);
+
+            }
+
+        }
         /*Create the thread and pass the socket descriptor*/
     }
 
