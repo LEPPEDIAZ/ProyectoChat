c0324ab3faea46ac5a88b26e283de47b6112182c
BALJ55 <byronamh@gmail.com>
2019-04-03 02:23:29 -0600

    ahora compila! pero no funciona xdxd

--- server.cpp
+++ server.cpp
@@ -28,6 +28,36 @@ using namespace std;
 
 User_Manager users[MAX_USERS_CONNECTED];
 
+
+void *handle_connection(void *sock_arg) {
+    int sockID = (int) sock_arg;
+
+    if (sockID < 0) {
+        Mensaje error_connect = new Mensaje(1);
+        error_connect.error_connection_json(1, "no se puedo conectar al servidor");
+        std::cout << error_connect.to_string() << endl;
+        cerr << "Error aceptando el request del client" << endl;
+        exit(1);
+    }
+    cout << "Cliente conectado!" << endl;
+    while (1) {
+
+        string mensaje = recibir_mensaje(sockID);
+        if (mensaje != "3312wazo") {
+            printf("hay un mensaje en cola!\n");
+            cout << "Mensajes: " << mensaje << "\n" << endl;
+            auto mensaje_parseado = json::parse(mensaje);
+            int code = mensaje_parseado["code"];
+            cout << "codigo fue: " << code << endl;
+            cout << "-----";
+
+            send(sockID, "Mensaje recibido!", strlen("Mensaje recibido!"), 0);
+        }
+    }
+
+}
+
+
 int main(int argc, char *argv[]) {
     struct sockaddr_in serv_addr, cli_addr;
     if (argc != 2) {
@@ -80,7 +110,7 @@ int main(int argc, char *argv[]) {
         int user_stack_id = users->add_user(new_username, 1, curr_time, accepted);
 
         if (user_stack_id != -1) {
-            if (pthread_create(&users->get_user_socket(user_stack_id), NULL, handle_connection, (void *) accepted) != 0) {
+            if (pthread_create(users->get_user_socket(user_stack_id), NULL, &handle_connection, (void *) accepted) != 0) {
 
                 perror("create thread");
                 exit(EXIT_FAILURE);
@@ -104,30 +134,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void *handle_connection(void *sock_arg) {
-    int sockID = (int) sock_arg;
-
-    if (sockID < 0) {
-        Mensaje error_connect = new Mensaje(1);
-        error_connect.error_connection_json(1, "no se puedo conectar al servidor");
-        std::cout << error_connect.to_string() << endl;
-        cerr << "Error aceptando el request del client" << endl;
-        exit(1);
-    }
-    cout << "Cliente conectado!" << endl;
-    while (1) {
-
-        string mensaje = recibir_mensaje(sockID);
-        if (mensaje != "3312wazo") {
-            printf("hay un mensaje en cola!\n");
-            cout << "Mensajes: " << mensaje << "\n" << endl;
-            auto mensaje_parseado = json::parse(mensaje);
-            int code = mensaje_parseado["code"];
-            cout << "codigo fue: " << code << endl;
-            cout << "-----";
-
-            send(sockID, "Mensaje recibido!", strlen("Mensaje recibido!"), 0);
-        }
-    }
-
-}
