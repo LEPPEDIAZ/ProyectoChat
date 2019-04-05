ca6a9e88bcf96a90273b9f90eb9049555c57155a
byronamh <byronamh@gmail.com>
2019-04-04 23:27:52 -0600

    validacion de longitud de request

--- server.cpp
+++ server.cpp
@@ -48,61 +48,64 @@ void *handle_connection(void *sock_arg) {
         string mensaje = recibir_mensaje(sockID);
         if (mensaje != "3312wazo") {
             cout << "Request: " << mensaje << "\n" << endl;
-            auto mensaje_parseado = json::parse(mensaje);
-            int code = mensaje_parseado["code"];
-            switch (code) {
-                //send message & boradcast
-                case 1:
+            if(strlen(mensaje)){
+                auto mensaje_parseado = json::parse(mensaje);
+                int code = mensaje_parseado["code"];
+                switch (code) {
+                    //send message & boradcast
+                    case 1:
 
-                    auto mensaje = mensaje_parseado["data"]["message"];
-                    auto targets = mensaje_parseado["data"]["to"];
+                        auto mensaje = mensaje_parseado["data"]["message"];
+                        auto targets = mensaje_parseado["data"]["to"];
 
-                    for (auto it = targets.begin(); it != targets.end(); it++) {
-                        Mensaje user_message = new Mensaje(201);
-                        user_message.send_message_success_json(users->get_username_by_index(*it),mensaje);
-                        enviar_mensaje(user_message.to_string(), users->get_user_socket(*it));
-                    }
-                    break;
-                case 3:
-                    auto requested_users = mensaje_parseado["data"]["user"];
-                    json response;
-                    response["code"] = 203;
-                    vector<string> users_list;
+                        for (auto it = targets.begin(); it != targets.end(); it++) {
+                            Mensaje user_message = new Mensaje(201);
+                            user_message.send_message_success_json(users->get_username_by_index(*it),mensaje);
+                            enviar_mensaje(user_message.to_string(), users->get_user_socket(*it));
+                        }
+                        break;
+                    case 3:
+                        auto requested_users = mensaje_parseado["data"]["user"];
+                        json response;
+                        response["code"] = 203;
+                        vector<string> users_list;
 
-                    for (auto user = requested_users.begin(); user != requested_users.end(); user++){
-                        json user_json;
-                        user_json['id'] = user;
-                        user_json['username'] = users->get_username_by_index(user);
-                        user_json['status'] = users->get_user_status_by_index(user);
-                        user_json['last_connected'] = users->get_user_last_connected_by_index(user);
+                        for (auto user = requested_users.begin(); user != requested_users.end(); user++){
+                            json user_json;
+                            user_json['id'] = user;
+                            user_json['username'] = users->get_username_by_index(user);
+                            user_json['status'] = users->get_user_status_by_index(user);
+                            user_json['last_connected'] = users->get_user_last_connected_by_index(user);
 
-                        users_list.push_back(user_json.dump());
-                    }
-                    response["data"]["user"] = users_list;
+                            users_list.push_back(user_json.dump());
+                        }
+                        response["data"]["user"] = users_list;
 
-                    enviar_mensaje(response.dump(),sockID);
+                        enviar_mensaje(response.dump(),sockID);
 
-                    break;
+                        break;
 //status change
-                case 4:
-                    auto new_user_status = mensaje_parseado["data"]["new_status"];
-                    auto user_index = mensaje_parseado["user"]["message"];
-                    for (auto it = user_index.begin(); it != user_index.end(); it++) {
-                        users->set_user_status(*it, new_user_status);
-                        Mensaje user_message = new Mensaje(204);
-                        user_message.build_success_json("cambio de status exitoso");
-                        enviar_mensaje(user_message.to_string(), users->get_user_socket(*it));
-                    }
-                    break;
-                case 5:
-                    auto username = mensaje_parseado["data"]["username"];
-                    users->whipe_user(users->search_user_by_username(username));
-                    break;
+                    case 4:
+                        auto new_user_status = mensaje_parseado["data"]["new_status"];
+                        auto user_index = mensaje_parseado["user"]["message"];
+                        for (auto it = user_index.begin(); it != user_index.end(); it++) {
+                            users->set_user_status(*it, new_user_status);
+                            Mensaje user_message = new Mensaje(204);
+                            user_message.build_success_json("cambio de status exitoso");
+                            enviar_mensaje(user_message.to_string(), users->get_user_socket(*it));
+                        }
+                        break;
+                    case 5:
+                        auto username = mensaje_parseado["data"]["username"];
+                        users->whipe_user(users->search_user_by_username(username));
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
 
+                }
             }
+
             //send(sockID, "Mensaje recibido!", strlen("Mensaje recibido!"), 0);
         }
     }
